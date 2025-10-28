c98ac7b36d7b2bd83c28fd932ecf59d3e1f24590
Deni Yusup <deni.yusup@bekaert.com>
2025-10-28 13:58:22 +0700

    update dashboard

--- ptw/resources/views/layouts/app.blade.php
+++ ptw/resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}?v={{ time() }}" rel="stylesheet">
 
     @yield('styles')
 
